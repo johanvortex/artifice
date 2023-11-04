505167e90bc3a5e3398b58f1e171366b964cd615
JOHAN ANDRES <johan-rm89@hotmail.com>
2023-11-04 11:01:42 -0500

    Codigo

--- src/components/start.js
+++ src/components/start.js
@@ -6,11 +6,16 @@ import { Carousel } from "react-responsive-carousel";
 import Imagen1 from "../assets/img/carru1.jpg";
 import Imagen2 from "../assets/img/carru2.jpg";
 import Imagen3 from "../assets/img/carru3.jpg";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { Grid } from "@mui/material";
 function App() {
   return (
     <div className="container">
-      <div className="app-header">
+      {/* <div className="app-header">
         <div className="logo">
           <img
             src={Logo}
@@ -24,12 +29,12 @@ function App() {
           <a href="/blog">Servicios</a>
           <a href="/media">Galerias</a>
           <a href="/shortcode">Contacto</a>
-          <div className="separator"></div> {/* Línea vertical de separación */}
+          <div className="separator"></div> 
           <a href="/search">Blog</a>
         </nav>
-      </div>
+      </div> */}
       <div className="section">
-        <div className="left-section">
+        {/* <div className="left-section">
           <img
             src={Logo}
             alt="Logo"
@@ -43,21 +48,68 @@ function App() {
             particulares, transformamos tus sueños en realidad. Bienvenido a
             artífice.
           </h3>
-        </div>
-        <div className="right-section">
-          <div className="carousel-container">
-            <Carousel showThumbs={false} showStatus={false}>
-              <div className="carousel-slide">
-                <img src={Imagen1} alt="Imagen 1" />
-              </div>
-              <div className="carousel-slide">
-                <img src={Imagen2} alt="Imagen 2" />
-              </div>
-              <div className="carousel-slide">
-                <img src={Imagen3} alt="Imagen 3" />
-              </div>
-            </Carousel>
-          </div>
+        </div> */}
+        <div className="carousel-container">
+          <Grid container spacing={2}>
+            <Grid item xs={12} lg={6}>
+                <div style={{ width: 400, height: 330, borderRadius: 20 }}>
+                  <Swiper
+                    slidesPerView={"auto"}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                      delay: 2500,
+                      disableOnInteraction: false,
+                    }}
+                    pagination={{
+                      clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                  >
+                    <SwiperSlide>
+                      <img src={Imagen1} alt="Imagen 1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <img src={Imagen2} alt="Imagen 1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <img src={Imagen3} alt="Imagen 1" />
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+                <div style={{ width: 400, height: 330, borderRadius: 20 }}>
+                  <Swiper
+                    slidesPerView={"auto"}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                      delay: 2500,
+                      disableOnInteraction: false,
+                    }}
+                    pagination={{
+                      clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper"
+                  >
+                    <SwiperSlide>
+                      <img src={Imagen1} alt="Imagen 1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <img src={Imagen2} alt="Imagen 1" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <img src={Imagen3} alt="Imagen 1" />
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
     </div>

a393ab582a5dd25af623cfb45176a4361cc1d6c5
3stebanT <lestebantriana2000@gmail.com>
2023-11-08 18:45:54 -0500

    coim

--- src/components/nosotros.js
+++ src/components/nosotros.js
@@ -13,6 +13,7 @@ function Nosotros() {
     <div className='contmision'>
           <div className='sectionN'>
             <div className='left-sectionN'> 
+            
             <h1>Misión</h1>
                 <hr/>
                 <h3>Transformar espacios en entornos inspiradores y funcionales, donde la belleza y la funcionalidad son conceptos que trabajan de manera armoniosa. Nos comprometemos a superar las expectativas de nuestros clientes a través de la creatividad, la innovación y un servicio excepcional. Guiados por la pasión por el diseño y la excelencia en la ejecución y calidad, creamos soluciones personalizadas que reflejan la visión única de cada cliente.</h3>
